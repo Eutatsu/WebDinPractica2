faa6a437eddca6b2390ae4e618f7da9f12fbec61
Eutatsu <84792485+Eutatsu@users.noreply.github.com>
2024-12-19 09:44:51 +0100

    muerte

---
+++ js/about.2fb4e6e5.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkprac2"]=self["webpackChunkprac2"]||[]).push([[594],{603:function(n,u,a){a.r(u),a.d(u,{default:function(){return o}});var r=a(641);const t={class:"about"};function c(n,u){return(0,r.uX)(),(0,r.CE)("div",t,u[0]||(u[0]=[(0,r.Lk)("h1",null,"This is an about page",-1)]))}var e=a(262);const s={},i=(0,e.A)(s,[["render",c]]);var o=i}}]);
+//# sourceMappingURL=about.2fb4e6e5.js.map
