5cc33d822c227b1e7bec68013fbd1af077d76f94
Eutatsu <eudald.inventor@gmail.com>
2024-12-25 16:11:26 +0100

    uouo

--- prac2/src/components/Mosaic.vue
+++ prac2/src/components/Mosaic.vue
@@ -5,16 +5,20 @@
             <div 
             v-if="colla.codi_color!=='#ffffff'"
             class="casella" 
+            :id="'colla'+index"
             :style="{backgroundColor:colla.codi_color}"
-            v-b-popover.hover.bottom="colla.nom">
+            >
             </div>
+            <b-popover :target="'colla'+index" triggers="hover" placement="bottom">
+               <p>{{colla.nom}}<br>{{ colla.color_camisa }}</p>
+            </b-popover>
         </div>
         <div v-for="(colla,index) in data" :key="index">
             <div 
             v-if="colla.codi_color=='#ffffff'"
             class="casella" 
             :style="{backgroundColor:colla.codi_color}"
-            v-b-popover.hover.bottom="colla.nom">
+            v-b-popover.hover.bottom="colla.nom<br>colla.nom_color">
             </div>
         </div>
     </div>
